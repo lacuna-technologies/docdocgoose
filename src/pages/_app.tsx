618919aae9fc46fd043415dd9242071a11ff5cbf
Huey <hello@huey.xyz>
2022-06-09 23:35:19 +0800

    fix: patch more methods
    Former-commit-id: 2fbebe2dc62bfaa05cffc300f10eac3806a6c8f5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,81 @@ import Filer from 'filer'
 
 import 'styles/globals.css'
 
-var Buffer = Filer.Buffer
-var fs = new Filer.FileSystem({ provider: new Filer.FileSystem.providers.Memory() })
+globalThis.fs = new Filer.FileSystem({ provider: new Filer.FileSystem.providers.Memory() })
+globalThis.fs.writeOriginal = globalThis.fs.write
+globalThis.fs.write = (fd, buf, offset, length, position, callback) => {
+  if(fd === 1 || fd === 2){ // stdout or stderr
+    const decoder = new TextDecoder("utf-8");
+    let outputBuf = "";
+    outputBuf += decoder.decode(buf);
+    const nl = outputBuf.lastIndexOf("\n");
+    if (nl != -1) {
+      console.log(outputBuf.substr(0, nl));
+      outputBuf = outputBuf.substr(nl + 1);
+    }
+    callback(null, buf.length, buf)
+  } else {
+    return globalThis.fs.writeOriginal(fd, buf, offset, length, position, callback)
+  }
+}
+globalThis.fs.writeSync = (fd, buf, offset, length, position) => {
+  let done = false
+  let result = null
+  while(!done){
+    globalThis.fs.write(fd, buf, offset, length, position, (err, bytesWritten, buffer) => {
+      if(err) {
+        throw err
+      }
+      done = true
+      result = bytesWritten
+    })
+  }
+  return result
+}
+globalThis.fs.openOriginal = globalThis.fs.open
+globalThis.fs.open = (path, flags, mode, callback) => {
+  let currentFlags = `r`
+  const constants = globalThis.fs.constants
+
+  if(flags === constants.O_RDONLY){
+    currentFlags = `r`
+  } else if(flags & constants.O_WRONLY) {
+    currentFlags = `w`
+  } else if (flags & constants.O_RDWR) {
+    if (flags & constants.O_CREAT && flags & constants.O_TRUNC) {
+      currentFlags = 'w+'
+    } else {
+      currentFlags = 'r+'
+    }
+  } else {
+    console.log(`flags`, flags)
+    throw new Error(`Unimplemented open flags`)
+  }
+
+  return globalThis.fs.openOriginal(path, currentFlags, mode, callback)
+}
+globalThis.fs.fstatOriginal = globalThis.fs.fstat
+globalThis.fs.fstat = (fd, callback) => {
+  return global.fs.fstatOriginal(fd, (err, stats) => {
+    let retStat = stats
+    delete retStat[`version`]
+    delete retStat[`filedata`]
+    retStat.dev = 0
+    retStat.ino = 0
+    retStat.rdev = 0
+    retStat.blksize = 4096
+    retStat.blocks = Math.ceil(retStat.size / 512)
+    retStat.atimeMs = retStat.atime.getTime()
+    retStat.mtimeMs = retStat.mtime.getTime()
+    retStat.ctimeMs = retStat.ctime.getTime()
+    retStat.birthtimeMs = retStat.ctimeMs
+    retStat.birthtime = retStat.ctime
+    retStat.nlink = retStat.nlinks
+    delete retStat['nlinks']
+    console.log(retStat)
+    return callback(err, retStat)
+  })
+}
 
 const DocsTogether = ({ Component, pageProps }: AppProps) => {
   const [wasmLoaded, setWasmLoaded] = useState(false)
@@ -15,7 +88,7 @@ const DocsTogether = ({ Component, pageProps }: AppProps) => {
     if(go === null){
       throw new Error(`Go is not loaded`)
     }
-    if(typeof fs === `undefined`){
+    if(typeof globalThis.fs === `undefined`){
       throw new Error(`fs is not patched`)
     }
     go.argv = [`pdfcpu.wasm`, ...params]
@@ -27,13 +100,10 @@ const DocsTogether = ({ Component, pageProps }: AppProps) => {
 
   useEffect(() => {
     if(wasmLoaded && go === null){
-      fetch(`/test.pdf`).then(res => {
+      fetch(`/test2.pdf`).then(res => {
         return res.arrayBuffer()
       }).then((buffer) => {
-        fs.writeFile(`/test.pdf`, Buffer.from(buffer), (err: any) => {
-          fs.readFile(`/test.pdf`, (e: any, contents: any) => {
-            console.log(contents)
-          })
+        globalThis.fs.writeFile(`/test2.pdf`, Buffer.from(buffer), (err: any) => {
         })
       })
       setGo(new Go())
@@ -43,7 +113,7 @@ const DocsTogether = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     if(go !== null){
       // runPdfCpu([`version`, `-c`, `disable`])
-      runPdfCpu([`properties`, `list`, `/test.pdf`, `-c`, `disable`])
+      runPdfCpu([`permissions`, `list`, `-v`, `/test2.pdf`])
     }
   }, [go])
 
